ac89511c3a943fe61a82b7130b99161cdeff313f
jxhnkndl <jkroyston@gmail.com>
2024-03-25 10:34:43 -0400

    create blog pages and dynamic route for loading blog posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Header from '@/src/components/header';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
       <Header />
