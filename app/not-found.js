89b3bb9a139ca28dfdca4d09cc7ca24333f2b5e8
jxhnkndl <jkroyston@gmail.com>
2024-03-26 10:07:59 -0400

    add custom 404 page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+// By adding this 404 page to the app directory, it will handle ALL 404s across the entire site; by contrast, putting it in a page folder would limit the not found page to only handling 404 errors related to the specific page
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  )
+}
