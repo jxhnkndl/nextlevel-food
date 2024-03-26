ac619b4e3fe9898a481c892ca21959d86d4d8bca
jxhnkndl <jkroyston@gmail.com>
2024-03-26 12:33:35 -0400

    set up useFormState hook on form and conditionally render server-side validation error messages

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,9 @@
+'use client';
+
+// Must be used in a client component
+// useFormState() manages the state of a page or component submitting a form using server actions
+import { useFormState } from 'react-dom';
+
 import { shareMeal } from '@/lib/actions';
 
 import ImagePicker from '@/components/meals/image-picker';
@@ -6,6 +12,11 @@ import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import styles from './page.module.css';
 
 export default function ShareMealPage() {
+  // Arg 1: The action that should be performed when the form is submitted
+  // Arg 2: The initial state of the response BEFORE the form has been submitted (matches up with the return object created in the server actions)
+  // The hook generates a state property representing the most recent form state and a formAction function for calling the action (shareMeal) when the form gets submitted
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={styles.header}>
@@ -15,8 +26,8 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        {/* Use server action function to submit form data */}
-        <form className={styles.form} action={shareMeal}>
+        {/* Use server action function to submit form data via the useFormState() hook */}
+        <form className={styles.form} action={formAction}>
           <div className={styles.row}>
             <p>
               <label htmlFor='name'>Your name</label>
@@ -44,6 +55,8 @@ export default function ShareMealPage() {
               required></textarea>
           </p>
           <ImagePicker label='Your image' name='image' />
+          {/* Conditionally render error message if one has been returned from the server action */}
+          {state.message && <p>{state.message}</p>}
           <p className={styles.actions}>
             <MealsFormSubmit />
           </p>
