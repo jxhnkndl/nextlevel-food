cf9225fcaf2c42533f941230871a4470854f2a7e
jxhnkndl <jkroyston@gmail.com>
2024-03-26 11:14:10 -0400

    add server action for submitting form data to the database

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,27 @@ import ImagePicker from '@/components/meals/image-picker';
 import styles from './page.module.css';
 
 export default function ShareMealPage() {
+  // The use server directive creates a SERVER ACTION that will only ever run on the server
+  // Server actions must be explicitly defined using 'use server'
+  // Server actions are always async functions
+  // This function will get called with the form gets submitted and will run directly on the server
+  // The formData interface will be used to grab the input values 
+  async function shareMeal(formData) {
+    'use server';
+
+    // .get() pulls out the value of a field from the formData object
+    const meal = {
+      title: formData.get('title'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email')
+    }
+
+    console.log(meal)
+  }
+
   return (
     <>
       <header className={styles.header}>
@@ -11,37 +32,37 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        {/* Use server action function to submit form data */}
+        <form className={styles.form} action={shareMeal}>
           <div className={styles.row}>
             <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <label htmlFor='name'>Your name</label>
+              <input type='text' id='name' name='name' required />
             </p>
             <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <label htmlFor='email'>Your email</label>
+              <input type='email' id='email' name='email' required />
             </p>
           </div>
           <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <label htmlFor='title'>Title</label>
+            <input type='text' id='title' name='title' required />
           </p>
           <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <label htmlFor='summary'>Short Summary</label>
+            <input type='text' id='summary' name='summary' required />
           </p>
           <p>
-            <label htmlFor="instructions">Instructions</label>
+            <label htmlFor='instructions'>Instructions</label>
             <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
-              required
-            ></textarea>
+              id='instructions'
+              name='instructions'
+              rows='10'
+              required></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
-            <button type="submit">Share Meal</button>
+            <button type='submit'>Share Meal</button>
           </p>
         </form>
       </main>
